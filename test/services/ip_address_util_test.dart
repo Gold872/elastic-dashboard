b519adab7313314229bf88ca79ad3c24c5ca39f9
Gold872 <thethunder428@gmail.com>
2023-08-17 17:50:01 -0400

    Add more ip address tests

--- test/services/ip_address_util_test.dart
+++ test/services/ip_address_util_test.dart
@@ -10,5 +10,8 @@ void main() {
     expect(IPAddressUtil.teamNumberToIP(47), '10.0.47.2');
     expect(IPAddressUtil.teamNumberToIP(3015), '10.30.15.2');
     expect(IPAddressUtil.teamNumberToIP(12053), '10.120.53.2');
+
+    expect(IPAddressUtil.getIpFromInt32Value(2130706433), '127.0.0.1');
+    expect(IPAddressUtil.getIpFromInt32Value(167982338), '10.3.53.2');
   });
 }
