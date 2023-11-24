07e4958ba9830349e3269c6ec81472aed1599dfd
Gold872 <thethunder428@gmail.com>
2023-11-23 23:35:14 -0500

    Make match time use maximum space

--- lib/widgets/nt4_widgets/single_topic/match_time.dart
+++ lib/widgets/nt4_widgets/single_topic/match_time.dart
@@ -3,6 +3,7 @@ import 'package:elastic_dashboard/services/globals.dart';
 import 'package:elastic_dashboard/services/nt4_connection.dart';
 import 'package:elastic_dashboard/widgets/nt4_widgets/nt4_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MatchTimeWidget extends StatelessWidget with NT4Widget {
   @override
@@ -37,16 +38,32 @@ class MatchTimeWidget extends StatelessWidget with NT4Widget {
 
   @override
   Widget build(BuildContext context) {
+    notifier = context.watch<NT4WidgetNotifier?>();
+
     return StreamBuilder(
       stream: subscription?.periodicStream(),
       initialData: nt4Connection.getLastAnnouncedValue(topic),
       builder: (context, snapshot) {
         double time = tryCast(snapshot.data) ?? -1.0;
 
-        return Text('${time.ceil()}',
-            style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                  color: _getTimeColor(time.ceil().toDouble()),
-                ));
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: FittedBox(
+                alignment: Alignment.center,
+                fit: BoxFit.contain,
+                child: Text(
+                  '${time.ceil()}',
+                  style: TextStyle(
+                    color: _getTimeColor(time.ceil().toDouble()),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
       },
     );
   }
