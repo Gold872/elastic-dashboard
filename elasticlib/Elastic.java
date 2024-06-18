d891f58806e9a64c87428ffcf8bd0957ff7c6b54
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2024-06-18 13:23:06 -0400

    add elasticlib

---
+++ elasticlib/Elastic.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.PubSubOptions;
+import edu.wpi.first.networktables.StringArrayEntry;
+import edu.wpi.first.networktables.StringArrayPublisher;
+import edu.wpi.first.networktables.StringArrayTopic;
+
+public final class Elastic {
+    private static final StringArrayTopic topic = NetworkTableInstance.getDefault().getStringArrayTopic("notifications");
+    private static final StringArrayPublisher publisher = topic.publish(PubSubOption.sendAll(true));
+
+    public static void sendAlert(AlertLevel level, String title, String description) {
+
+        publisher.set(new String[] {level.toString(), title, description});
+        System.out.println("sent alert");
+    }
+    public enum AlertLevel {
+        INFO,
+        WARNING,
+        ERROR
+    }
+}
