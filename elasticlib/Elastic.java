983d8e878ba2c00476933e5d5e0b59d13ef55370
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2024-06-24 20:42:12 -0400

    make elastic robot notification variables mutable

--- elasticlib/Elastic.java
+++ elasticlib/Elastic.java
@@ -33,12 +33,24 @@ public final class Elastic {
             this.description = description;
         }
 
+        public void setLevel(NotificationLevel level) {
+            this.level = level;
+        }
+
         public NotificationLevel getLevel() {
             return level;
         }
 
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
         public String getTitle() {
             return title;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
         }
 
         public String getDescription() {
@@ -51,5 +63,4 @@ public final class Elastic {
             ERROR
         }
     }
-
 }
