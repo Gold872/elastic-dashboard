59a8f1a97dd69408cb40a8ea14b7802ef2108059
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-07-02 20:53:05 +0300

    added option to show bandwidth in camera stream

--- lib/widgets/mjpeg.dart
+++ lib/widgets/mjpeg.dart
@@ -1,24 +1,28 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:http/http.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
+/// Notifier class to manage the state of the MJPEG widget.
 class _MjpegStateNotifier extends ChangeNotifier {
   bool _mounted = true;
   bool _visible = true;
 
   _MjpegStateNotifier() : super();
 
+  /// Getter for mounted state.
   bool get mounted => _mounted;
 
+  /// Getter for visibility state.
   bool get visible => _visible;
 
-  set visible(value) {
+  /// Setter for visibility state.
+  set visible(bool value) {
     _visible = value;
     notifyListeners();
   }
@@ -33,10 +37,11 @@ class _MjpegStateNotifier extends ChangeNotifier {
 
 /// A preprocessor for each JPEG frame from an MJPEG stream.
 class MjpegPreprocessor {
+  /// Processes a JPEG frame.
   List<int>? process(List<int> frame) => frame;
 }
 
-/// An Mjpeg.
+/// Widget to display an MJPEG stream.
 class Mjpeg extends HookWidget {
   final streamKey = UniqueKey();
   final MjpegStreamState mjpegStream;
@@ -44,9 +49,12 @@ class Mjpeg extends HookWidget {
   final double? width;
   final double? height;
   final WidgetBuilder? loading;
-  final Widget Function(BuildContext contet, dynamic error, dynamic stack)?
+  final bool showBandwidthUsage;
+
+  final Widget Function(BuildContext context, dynamic error, dynamic stack)?
       error;
 
+  /// Constructor for the Mjpeg widget.
   Mjpeg({
     required this.mjpegStream,
     this.width,
@@ -54,6 +62,7 @@ class Mjpeg extends HookWidget {
     this.fit,
     this.error,
     this.loading,
+    required this.showBandwidthUsage,
     super.key,
   });
 
@@ -63,19 +72,21 @@ class Mjpeg extends HookWidget {
     final state = useMemoized(() => _MjpegStateNotifier());
     final visible = useListenable(state);
     final errorState = useState<List<dynamic>?>(null);
-    isMounted() => context.mounted;
+
+    bool isMounted() => context.mounted;
 
     final manager = useMemoized(
-        () => _StreamManager(
-              mjpegStream: mjpegStream,
-              mounted: isMounted,
-              visible: () => visible.visible,
-            ),
-        [
-          visible.visible,
-          isMounted(),
-          mjpegStream,
-        ]);
+      () => _StreamManager(
+        mjpegStream: mjpegStream,
+        mounted: isMounted,
+        visible: () => visible.visible,
+      ),
+      [
+        visible.visible,
+        isMounted(),
+        mjpegStream,
+      ],
+    );
 
     final key = useMemoized(() => UniqueKey(), [manager]);
 
@@ -118,30 +129,39 @@ class Mjpeg extends HookWidget {
               ? const Center(child: CircularProgressIndicator())
               : loading!(context));
     }
+    double bandwidth = mjpegStream.bandwidth;
 
-    return VisibilityDetector(
-      key: key,
-      child: Image(
-        image: image.value ?? mjpegStream.previousImage!,
-        width: width,
-        height: height,
-        gaplessPlayback: true,
-        fit: fit,
-      ),
-      onVisibilityChanged: (VisibilityInfo info) {
-        if (visible.mounted) {
-          visible.visible = info.visibleFraction != 0;
-        }
-      },
+    return Column(
+      children: [
+        VisibilityDetector(
+          key: key,
+          child: Image(
+            image: image.value ?? mjpegStream.previousImage!,
+            width: width,
+            height: height,
+            gaplessPlayback: true,
+            fit: fit,
+          ),
+          onVisibilityChanged: (VisibilityInfo info) {
+            if (visible.mounted) {
+              visible.visible = info.visibleFraction != 0;
+            }
+          },
+        ),
+        showBandwidthUsage
+            ? Text('Bandwidth usage: ${bandwidth.toStringAsFixed(2)} KB/s',
+                style: const TextStyle(fontSize: 13))
+            : const SizedBox(height: 5),
+      ],
     );
   }
 }
 
+/// State management for MJPEG stream.
 class MjpegStreamState {
   static const _trigger = 0xFF;
   static const _soi = 0xD8;
   static const _eoi = 0xD9;
-
   final String stream;
   final bool isLive;
   final Duration timeout;
@@ -161,6 +181,7 @@ class MjpegStreamState {
 
   late final Timer bandwidthTimer;
 
+  /// Constructor for MjpegStreamState.
   MjpegStreamState({
     required this.stream,
     this.isLive = true,
@@ -170,11 +191,11 @@ class MjpegStreamState {
   }) {
     bandwidthTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
       bandwidth = bitCount / 1e6;
-
       bitCount = 0;
     });
   }
 
+  /// Disposes the resources.
   void dispose() {
     for (StreamSubscription subscription in _subscriptions.values) {
       subscription.cancel();
@@ -188,31 +209,24 @@ class MjpegStreamState {
     bitCount = 0;
   }
 
+  /// Cancels the subscription for the given key.
   void cancelSubscription(Key key) {
     if (_subscriptions.containsKey(key)) {
       _subscriptions.remove(key)!.cancel();
-
       if (_subscriptions.isEmpty) {
         dispose();
       }
     }
   }
 
+  /// Sends the image to the provided ValueNotifier.
   void sendImage(
     ValueNotifier<MemoryImage?> image,
     ValueNotifier<dynamic> errorState,
     List<int> chunks, {
     required bool Function() mounted,
   }) async {
-    // pass image through preprocessor sending to [Image] for rendering
-    final List<int>? imageData;
-
-    if (preprocessor != null) {
-      imageData = preprocessor?.process(chunks);
-    } else {
-      imageData = chunks;
-    }
-
+    final List<int>? imageData = preprocessor?.process(chunks) ?? chunks;
     if (imageData == null) return;
 
     final imageMemory = MemoryImage(Uint8List.fromList(imageData));
@@ -224,6 +238,7 @@ class MjpegStreamState {
     }
   }
 
+  /// Handles data received from the stream.
   void _onDataReceived({
     required List<int> carry,
     required List<int> chunk,
@@ -231,15 +246,11 @@ class MjpegStreamState {
     required ValueNotifier<List<dynamic>?> errorState,
     required bool Function() mounted,
   }) async {
-    if (carry.isNotEmpty && carry.last == _trigger) {
-      if (chunk.first == _eoi) {
-        carry.add(chunk.first);
-        sendImage(image, errorState, carry, mounted: mounted);
-        carry = [];
-        if (!isLive) {
-          dispose();
-        }
-      }
+    if (carry.isNotEmpty && carry.last == _trigger && chunk.first == _eoi) {
+      carry.add(chunk.first);
+      sendImage(image, errorState, carry, mounted: mounted);
+      carry.clear();
+      if (!isLive) dispose();
     }
 
     for (var i = 0; i < chunk.length - 1; i++) {
@@ -247,17 +258,14 @@ class MjpegStreamState {
       final d1 = chunk[i + 1];
 
       if (d == _trigger && d1 == _soi) {
-        carry = [];
+        carry.clear();
         carry.add(d);
       } else if (d == _trigger && d1 == _eoi && carry.isNotEmpty) {
         carry.add(d);
         carry.add(d1);
-
         sendImage(image, errorState, carry, mounted: mounted);
-        carry = [];
-        if (!isLive) {
-          dispose();
-        }
+        carry.clear();
+        if (!isLive) dispose();
       } else if (carry.isNotEmpty) {
         carry.add(d);
         if (i == chunk.length - 2) {
@@ -267,6 +275,7 @@ class MjpegStreamState {
     }
   }
 
+  /// Updates the stream with the given key and notifiers.
   void updateStream(
     Key key,
     ValueNotifier<MemoryImage?> image,
@@ -278,12 +287,10 @@ class MjpegStreamState {
       try {
         final request = Request('GET', Uri.parse(stream));
         request.headers.addAll(headers);
-        final response = await httpClient.send(request).timeout(
-            timeout); //timeout is to prevent process to hang forever in some case
+        final response = await httpClient.send(request).timeout(timeout);
 
         if (response.statusCode >= 200 && response.statusCode < 300) {
           byteStream = response.stream.asBroadcastStream();
-
           _bitSubscription = byteStream!.listen((data) {
             bitCount += data.length * Uint8List.bytesPerElement * 8;
           });
@@ -298,7 +305,6 @@ class MjpegStreamState {
           dispose();
         }
       } catch (error, stack) {
-        // we ignore those errors in case play/pause is triggers
         if (!error
             .toString()
             .contains('Connection closed before full header was received')) {
@@ -309,42 +315,37 @@ class MjpegStreamState {
         }
       }
     }
-
-    if (byteStream == null) {
-      return;
-    }
+    if (byteStream == null) return;
 
     var carry = <int>[];
     _subscriptions.putIfAbsent(
-        key,
-        () => byteStream!.listen((chunk) {
-              if (!visible() || !mounted()) {
-                carry.clear();
-                return;
-              }
-              _onDataReceived(
-                carry: carry,
-                chunk: chunk,
-                image: image,
-                errorState: errorState,
-                mounted: mounted,
-              );
-            }, onError: (error, stack) {
-              try {
-                if (mounted()) {
-                  errorState.value = [error, stack];
-                  image.value = null;
-                }
-              } finally {
-                dispose();
-              }
-            }, cancelOnError: true));
+      key,
+      () => byteStream!.listen((chunk) {
+        if (!visible() || !mounted()) {
+          carry.clear();
+          return;
+        }
+        _onDataReceived(
+          carry: carry,
+          chunk: chunk,
+          image: image,
+          errorState: errorState,
+          mounted: mounted,
+        );
+      }, onError: (error, stack) {
+        if (mounted()) {
+          errorState.value = [error, stack];
+          image.value = null;
+        }
+        dispose();
+      }),
+    );
   }
 }
 
+/// Manages the MJPEG stream for the widget.
 class _StreamManager {
   final MjpegStreamState mjpegStream;
-
   final bool Function() mounted;
   final bool Function() visible;
 
@@ -354,9 +355,17 @@ class _StreamManager {
     required this.visible,
   });
 
-  void updateStream(Key key, ValueNotifier<MemoryImage?> image,
-      ValueNotifier<List<dynamic>?> errorState) async {
-    mjpegStream.updateStream(key, image, errorState,
-        visible: visible, mounted: mounted);
-  }
+  /// Updates the stream with the given key and notifiers.
+  void updateStream(
+    Key key,
+    ValueNotifier<MemoryImage?> image,
+    ValueNotifier<List<dynamic>?> errorState,
+  ) =>
+      mjpegStream.updateStream(
+        key,
+        image,
+        errorState,
+        visible: visible,
+        mounted: mounted,
+      );
 }
