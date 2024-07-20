52585059eaf6467f3f002e3f92dfaab43fadb4b5
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-07-20 17:07:46 -0400

    Fix first notification not showing

--- test/services/robot_notifications_listener_test.dart
+++ test/services/robot_notifications_listener_test.dart
@@ -39,8 +39,8 @@ void main() {
     verifyNever(mockOnNotification.call(any, any, any));
   });
 
-  test("Robot Notifications (Initial Connection | Existing Data) ", () {
-    MockNTConnection mockConnection = createMockOnlineNT4();
+  test("Robot Notifications (Initial Connection | Old Existing Data) ", () {
+    MockNTConnection mockConnection = createMockOnlineNT4(serverTime: 5000000);
     MockNT4Subscription mockSub = MockNT4Subscription();
 
     Map<String, dynamic> data = {
@@ -88,9 +88,6 @@ void main() {
         .called(1);
     verify(mockConnection.addDisconnectedListener(any)).called(1);
 
-    // Verify that no other interactions have been made with the mockConnection
-    verifyNoMoreInteractions(mockConnection);
-
     // Verify that the onNotification callback was never called
     verifyNever(mockOnNotification(any, any, any));
 
@@ -110,5 +107,67 @@ void main() {
     mockSub.updateValue(jsonEncode(data), 3);
     reset(mockOnNotification);
     verifyNever(mockOnNotification(any, any, any));
+
+    // Try with missing data
+    data.remove('level');
+    data['title'] = null;
+    data['description'] = null;
+
+    mockSub.updateValue(jsonEncode(data), 4);
+    reset(mockOnNotification);
+    verifyNever(mockOnNotification(any, any, any));
+  });
+
+  test("Robot Notifications (Initial Connection | Newer Existing Data) ", () {
+    MockNTConnection mockConnection = createMockOnlineNT4(serverTime: 5000000);
+    MockNT4Subscription mockSub = MockNT4Subscription();
+
+    Map<String, dynamic> data = {
+      'title': 'Title1',
+      'description': 'Description1',
+      'level': 'Info'
+    };
+
+    List<Function(Object?, int)> listeners = [];
+    when(mockSub.listen(any)).thenAnswer(
+      (realInvocation) {
+        listeners.add(realInvocation.positionalArguments[0]);
+        mockSub.updateValue(jsonEncode(data), 5000000);
+      },
+    );
+
+    when(mockSub.updateValue(any, any)).thenAnswer(
+      (invoc) {
+        for (var value in listeners) {
+          value.call(
+              invoc.positionalArguments[0], invoc.positionalArguments[1]);
+        }
+      },
+    );
+
+    when(mockConnection.subscribeAll(any, any)).thenAnswer(
+      (realInvocation) {
+        mockSub.updateValue(jsonEncode(data), 0);
+        return mockSub;
+      },
+    );
+
+    // Create a mock for the onNotification callback
+    MockNotificationCallback mockOnNotification = MockNotificationCallback();
+
+    RobotNotificationsListener notifications = RobotNotificationsListener(
+      ntConnection: mockConnection,
+      onNotification: mockOnNotification.call,
+    );
+
+    notifications.listen();
+
+    // Verify that subscribeAll was called with the specific parameters
+    verify(mockConnection.subscribeAll('/Elastic/robotnotifications', 0.2))
+        .called(1);
+    verify(mockConnection.addDisconnectedListener(any)).called(1);
+
+    // Verify that the onNotification callback was called
+    verify(mockOnNotification(any, any, any));
   });
 }
