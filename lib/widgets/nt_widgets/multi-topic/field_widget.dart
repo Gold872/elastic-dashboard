e931c967082146df83c7d3bb89c6d3765e15f1ea
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-08-04 10:16:59 +0300

    removed logs

--- lib/widgets/nt_widgets/multi-topic/field_widget.dart
+++ lib/widgets/nt_widgets/multi-topic/field_widget.dart
@@ -97,9 +97,6 @@ class FieldWidgetModel extends NTWidgetModel {
   void init() {
     super.init();
 
-    logger.info(Colors.red.toHexString());
-    logger.info(Colors.white.toHexString());
-
     _robotTopicName = '$topic/Robot';
 
     topicAnnounceListener = (nt4Topic) {
