66d6d8296f94f55346aed132a5682d09c6a9ff32
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-10-14 08:37:58 +0300

    Added elasticlib c++

---
+++ elasticlib/C++/include/elasticlib.h
@@ -0,0 +1,84 @@
+
+#pragma once
+
+#include <iostream>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/StringTopic.h>
+#include <ntcore_cpp.h>
+#include <string>
+#include <wpi/json.h>
+
+namespace elastic {
+
+class NotificationHandler {
+public:
+  NotificationHandler() {
+    topic = nt::NetworkTableInstance::GetDefault().GetStringTopic(
+        "/Elastic/RobotNotifications");
+    publisher = topic.Publish(nt::PubSubOption::SendAll(true),
+                              nt::PubSubOption::KeepDuplicates(true));
+  }
+
+  struct Notification {
+    enum class Level { INFO, WARNING, ERROR };
+
+    Notification(Level level, const std::string &title,
+                 const std::string &description)
+        : level(level), title(title), description(description) {}
+
+    void SetLevel(Level level) { this->level = level; }
+
+    Level GetLevel() const { return level; }
+
+    void SetTitle(const std::string &title) { this->title = title; }
+
+    std::string GetTitle() const { return title; }
+
+    void SetDescription(const std::string &description) {
+      this->description = description;
+    }
+
+    std::string GetDescription() const { return description; }
+
+    std::string ToJson() const {
+      wpi::json jsonData;
+      jsonData["level"] = LevelToString(level);
+      jsonData["title"] = title;
+      jsonData["description"] = description;
+      return jsonData.dump();
+    }
+
+    static std::string LevelToString(Level level) {
+      switch (level) {
+      case Level::INFO:
+        return "INFO";
+      case Level::WARNING:
+        return "WARNING";
+      case Level::ERROR:
+        return "ERROR";
+      default:
+        return "UNKNOWN";
+      }
+    }
+
+  private:
+    Level level;
+    std::string title;
+    std::string description;
+  };
+
+  void SendAlert(const Notification &alert) {
+    try {
+      std::string jsonString = alert.ToJson();
+      publisher.Set(jsonString);
+    } catch (const std::exception &e) {
+      std::cerr << "Error processing JSON: " << e.what() << std::endl;
+    }
+  }
+
+private:
+  nt::StringTopic topic;
+  nt::StringPublisher publisher;
+};
+
+} // namespace elastic
