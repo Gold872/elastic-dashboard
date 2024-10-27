b3c9438607c1d8a53b468e47e92e6c2a830f64e6
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-10-27 15:40:19 -0400

    Fixed import sorting

--- test/services/shuffleboard_nt_listener_test.dart
+++ test/services/shuffleboard_nt_listener_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
