fc37de50709643cf95923b6428f8175fc4f477bb
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-19 11:22:43 -0500

    Added unit test for search query

--- test/widgets/network_tree/networktables_tree_test.dart
+++ test/widgets/network_tree/networktables_tree_test.dart
@@ -126,4 +126,54 @@ void main() {
     expect(find.text('String'), findsOneWidget);
     expect(find.text('string'), findsOneWidget);
   });
+
+  testWidgets('Network Tables Tree searching', (widgetTester) async {
+    NTConnection ntConnection = createMockOnlineNT4(
+      virtualTopics: [
+        NT4Topic(
+          name: '/Testing/Integer',
+          type: NT4TypeStr.kInt,
+          properties: {},
+        ),
+        NT4Topic(
+          name: '/Testing/Double',
+          type: NT4TypeStr.kFloat64,
+          properties: {},
+        ),
+        NT4Topic(
+          name: '/Testing/SubTable/String',
+          type: NT4TypeStr.kString,
+          properties: {},
+        ),
+      ],
+    );
+
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: NetworkTableTree(
+            ntConnection: ntConnection,
+            preferences: preferences,
+            hideMetadata: false,
+            searchQuery: 'Double',
+          ),
+        ),
+      ),
+    );
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text('Testing'), findsOneWidget);
+    await widgetTester.tap(find.text('Testing'));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text('Integer'), findsNothing);
+    expect(find.text('int'), findsNothing);
+
+    expect(find.text('Double'), findsOneWidget);
+    expect(find.text('double'), findsOneWidget);
+
+    expect(find.text('String'), findsNothing);
+    expect(find.text('string'), findsNothing);
+    expect(find.text('SubTable'), findsNothing);
+  });
 }
