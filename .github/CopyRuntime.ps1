53026c0691794c0e416be3d3490878efb45dda01
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-01-02 17:00:57 -0500

    Throw error if no files are copied

--- .github/CopyRuntime.ps1
+++ .github/CopyRuntime.ps1
@@ -19,5 +19,12 @@ $RuntimeLocation = Join-Path $VsDirectory "VC" "Redist" "MSVC" $ExpectedVersion 
 
 $CrtLocation = Join-Path $RuntimeLocation "*.CRT" -Resolve
 
-Copy-Item -Path "$CrtLocation\*.dll" -Destination $OutputPath
+if ($CrtLocation.Length -eq 0) {
+    throw [System.IO.FileNotFoundException] "$RuntimeLocation CRT folder not resolved"
+}
 
+$Copied = Copy-Item -Path "$CrtLocation\*.dll" -Destination $OutputPath -PassThru
+
+if ($Copied.Length -eq 0) {
+    throw [System.IO.FileNotFoundException] "$CrtLocation No files copied"
+}
