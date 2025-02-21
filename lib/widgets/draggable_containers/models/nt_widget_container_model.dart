2c69ad89b871c04011af16a2d59af899a98e3a21
Angad Tendulkar <angad.tendulkar@gmail.com>
2025-02-21 11:41:26 -0500

    fix serde on non-struct values

--- lib/widgets/draggable_containers/models/nt_widget_container_model.dart
+++ lib/widgets/draggable_containers/models/nt_widget_container_model.dart
@@ -1,3 +1,5 @@
+import 'package:elastic_dashboard/services/log.dart';
+import 'package:elastic_dashboard/services/nt4_client.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dot_cast/dot_cast.dart';
