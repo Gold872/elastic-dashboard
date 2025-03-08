cf6a8b4334c97dfb17ca9150d677eab9f566b562
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-03-08 11:10:49 -0500

    Cleanup code for detecting unit tests

--- lib/widgets/nt_widgets/multi_topic/field_widget.dart
+++ lib/widgets/nt_widgets/multi_topic/field_widget.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
 
@@ -14,6 +13,7 @@ import 'package:elastic_dashboard/services/field_images.dart';
 import 'package:elastic_dashboard/services/nt4_client.dart';
 import 'package:elastic_dashboard/services/struct_schemas/pose2d_struct.dart';
 import 'package:elastic_dashboard/services/text_formatter_builder.dart';
+import 'package:elastic_dashboard/util/test_utils.dart';
 import 'package:elastic_dashboard/widgets/dialog_widgets/dialog_color_picker.dart';
 import 'package:elastic_dashboard/widgets/dialog_widgets/dialog_dropdown_chooser.dart';
 import 'package:elastic_dashboard/widgets/dialog_widgets/dialog_text_input.dart';
@@ -671,8 +671,7 @@ class FieldWidget extends NTWidget {
 
             // Try rebuilding again if the image isn't fully rendered
             // Can't do it if it's in a unit test cause it causes issues with timers running
-            if (!model.rendered &&
-                !Platform.environment.containsKey('FLUTTER_TEST')) {
+            if (!model.rendered && !isUnitTest) {
               Future.delayed(const Duration(milliseconds: 100), model.refresh);
             }
 
