a96a54b862baaab4f0ce4050277190b2763586b6
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-03-08 12:30:40 -0500

    Unbroke everything

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -6,7 +6,7 @@ import 'package:titlebar_buttons/titlebar_buttons.dart';
 import 'package:elastic_dashboard/services/settings.dart';
 
 import 'package:window_manager/window_manager.dart'
-    if (kIsWeb) 'package:elastic_dashboard/util/window_stub.dart';
+    if (dart.library.js_interop) 'package:elastic_dashboard/util/window_stub.dart';
 
 /// Essentially a copy of Flutter's [AppBar] but with a non-fixed leading
 /// width and all non-necessary features removed for copying simplicity
