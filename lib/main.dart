d0393a20f3f6a784ec0e0708f6e1376da767995a
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-03-08 00:43:20 -0500

    Disable context menu on web

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:collection/collection.dart';
 import 'package:dot_cast/dot_cast.dart';
@@ -27,6 +28,10 @@ import 'package:window_manager/window_manager.dart'
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    BrowserContextMenu.disableContextMenu();
+  }
 
   // final PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
