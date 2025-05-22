aadcb64f18d8ebe7079841a4511262815d4e84cb
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-05-21 22:06:19 -0400

    Always use Canvaskit

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,12 +1,8 @@
-if (typeof ImageDecoder === "undefined") {
-  // Enables WASM to run in environments that don't support ImageDecoder
-  // Notably, this allows it to run in insecure contexts like on a robot
-  window.ImageDecoder = () => {};
-}
 // prettier-ignore
 {{flutter_js}}
 // prettier-ignore
 {{flutter_build_config}}
 
-_flutter.buildConfig.useLocalCanvasKit = true;
-_flutter.loader.load({ renderer: "skwasm" });
+_flutter.loader.load({
+  config: { renderer: "canvaskit", canvasKitBaseUrl: "/canvaskit/" },
+});
