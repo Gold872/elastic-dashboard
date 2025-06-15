01c9bb9589483dab437d9d8626ad51469f8f0112
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-06-15 11:21:34 -0400

    Convert double quotes to single and fix import sorting

--- test/widgets/nt_widgets/multi_topic/fms_info_test.dart
+++ test/widgets/nt_widgets/multi_topic/fms_info_test.dart
@@ -1,4 +1,3 @@
-import 'package:elastic_dashboard/services/nt4_type.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -6,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:elastic_dashboard/services/nt4_client.dart';
+import 'package:elastic_dashboard/services/nt4_type.dart';
 import 'package:elastic_dashboard/services/nt_connection.dart';
 import 'package:elastic_dashboard/services/nt_widget_builder.dart';
 import 'package:elastic_dashboard/widgets/nt_widgets/multi_topic/fms_info.dart';
