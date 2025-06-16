cff1c7420784ab9440f3163437407e2731548c89
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-06-16 19:28:59 -0400

    Add IPK build for systemcore

---
+++ web/start_elastic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+
+PORT = 4902
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
