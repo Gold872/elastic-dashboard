1fed53097a2514ddba5a1ea745ae4f6acb8706ae
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-11-20 23:41:34 -0500

    Add comment explaining the unloadHandler function

--- lib/util/unload_handler.dart
+++ lib/util/unload_handler.dart
@@ -3,6 +3,7 @@ import 'dart:js_interop';
 import 'package:web/web.dart';
 
 void setupUnloadHandler(bool Function() shouldShowWarning) {
+  // Using `toJSCaptureThis` requires a function to have a specific return type
   bool? unloadHandler(BeforeUnloadEvent event) {
     if (shouldShowWarning()) {
       return true;
