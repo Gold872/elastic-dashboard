d725321063a56228afc629286b496555459350b0
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-11-20 23:58:55 -0500

    Upgrade Flutter to 3.38.2 (#311)

--- lib/util/unload_handler.dart
+++ lib/util/unload_handler.dart
@@ -3,12 +3,15 @@ import 'dart:js_interop';
 import 'package:web/web.dart';
 
 void setupUnloadHandler(bool Function() shouldShowWarning) {
-  window.onbeforeunload = (event) {
-    event as BeforeUnloadEvent;
+  // Using `toJSCaptureThis` requires a function to have a specific return type
+  bool? unloadHandler(BeforeUnloadEvent event) {
     if (shouldShowWarning()) {
       return true;
     }
-  }.toJSCaptureThis;
+    return null;
+  }
+
+  window.onbeforeunload = unloadHandler.toJSCaptureThis;
 }
 
 void removeUnloadHandler() {
